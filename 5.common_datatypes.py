94c76276ab1791d5ef659ecca6bf033e82e937c8
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-19 02:22:00 +0800

    asdsa
    dad

---
+++ 5.common_datatypes.py
@@ -0,0 +1,19 @@
+#string datatype
+name = "Arnold"
+print("variable name datatype = ",type(name))
+
+#int 
+number1 = 1
+print("variable number1 datatype = ",type(number1))
+
+#float
+number2 = 1.0 
+print("variable number2 datatype = ",type(number2))
+
+#boolean 
+statement1 = True
+print("variable statement1 datatype = ",type(statement1))
+
+#List or array
+list_1 = ["apple", "banana" , "mango"]
+print("variable list_1 datatype = ",type(statement1))
