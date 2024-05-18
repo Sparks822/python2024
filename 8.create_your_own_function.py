7cb2b674d69790c439d682a9c344df5df7205891
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-19 03:08:06 +0800

    2131
    12313

--- 8.create_your_own_function.py
+++ 8.create_your_own_function.py
@@ -13,3 +13,21 @@ def add_two_variables(variable1, variable2):
 
 result = add_two_variables(10, 10)
 print("Function output:", result)
+
+
+# 1. Elements
+#    1. Elements: The individual items contained within the list.
+#    2. Index: The position of each element within the list, starting from 0.
+#    3. Length: The total number of elements in the list.
+
+# The elements are the items within the list. In this example, "apple", "banana", and "tomato" are the elements of the list x.
+
+# 2. Index
+# Each element in the list has an index, which is its position within the list:
+#     • x[0] is "apple"
+#     • x[1] is "banana"
+#     • x[2] is "tomato"
+
+# 3. Length
+
+# The length of a list is the number of elements it contains, which can be found using the len() function:
