fb5cea38c69f1d319d6eded3d37abd153881c2cd
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-19 02:11:21 +0800

    asds
    adsa

--- 3.check_datatype.py
+++ 3.check_datatype.py
@@ -1,7 +1,9 @@
 name = "Arnold"
 print("variable 'name' datatype is  : ", type(name))  
+#Check the datatype for name 
 
 name1 = "Arnalyn"
 print("name1", type(name1))
+#Check the datatyoe for name1
 
 #using the function type() to find the var
