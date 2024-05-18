37ecb6131315b99f1b0ee6bfd32ed5140aa2cc2e
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-19 03:02:42 +0800

    asda
    ads

---
+++ 8.create_your_own_function.py
@@ -0,0 +1,15 @@
+def add_two_variables(variable1, variable2):
+    """
+    Add two variables together.
+
+    Parameters:
+        variable1: The first variable.
+        variable2: The second variable.
+
+    Returns:
+        The sum of the two variables.
+    """
+    return variable1 + variable2
+
+result = add_two_variables(10, 10)
+print("Function output:", result)
