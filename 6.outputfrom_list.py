b752d4f6b9bbfe85d0991bae7ed90fc22263e8d2
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-19 03:08:58 +0800

    21
    1312

--- 6.outputfrom_list.py
+++ 6.outputfrom_list.py
@@ -6,3 +6,20 @@ print(x[2])  # Accesses and prints the third element of the list
 # x[0] accesses the first element of the list, which is "apple".
 # x[1] accesses the second element of the list, which is "banana".
 # x[2] accesses the third element of the list, which is "tomato".
+
+# 1. Elements
+#    1. Elements: The individual items contained within the list.
+#    2. Index: The position of each element within the list, starting from 0.
+#    3. Length: The total number of elements in the list.
+
+# The elements are the items within the list. In this example, "apple", "banana", and "tomato" are the elements of the list x.
+
+# 2. Index
+# Each element in the list has an index, which is its position within the list:
+#     • x[0] is "apple"
+#     • x[1] is "banana"
+#     • x[2] is "tomato"
+
+# 3. Length
+
+# The length of a list is the number of elements it contains, which can be found using the len() function:
