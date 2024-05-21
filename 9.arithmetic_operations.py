09c56f56944c9b427f7556d63c30844cc4e68478
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-22 06:00:15 +0800

    sad
    sda

---
+++ 9.arithmetic_operations.py
@@ -0,0 +1,29 @@
+#addition 
+num1 = 25
+num2 = 25
+sum = num1 + num2 
+print("Additition")
+print("25 + 25 =", sum)
+
+#subtraction 
+
+num1 = 25
+num2 = 25
+diff = num1 - num2 
+print("Subtraction")
+print("25 - 25 =", diff)
+
+
+#multiplication 
+num1 = 25
+num2 = 25
+mul = num1 * num2 
+print("multiplication")
+print("25 * 25 =", mul)
+
+#division 
+num1 = 500
+num2 = 25
+div = num1 / num2 
+print("multiplication")
+print("500 / 25 =", div)
