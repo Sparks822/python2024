769fc7125f7d64e296cdd7e479f3d72dcf4fc3a4
sparks822 <69517176+Sparks822@users.noreply.github.com>
2024-05-22 15:52:41 +0800

    Assignment Operators
    Assignment Operators

---
+++ 10assignment_operators.py
@@ -0,0 +1,27 @@
+print("The variable x = 3 ")
+x = 30 
+x += 3 
+print("x += 10 = ",x )
+
+x *= 3
+print("x *= 3 = ",x )
+
+x /= 3
+print("x /= 3 = ",x )
+
+x //= 3
+print("x //= 3 = ",x )
+
+x %= 3
+print("x %= 3 = ",x )
+
+x **= 3
+print("x **= 3 = ",x )
+
+#x += 3 (add 3 to x)
+#x -= 3 (subtract 3 from x)
+#x *= 3 (multiply x by 3)
+#x /= 3 (divide x by 3)
+#x //= 3 (integer division of x by 3)
+#x %= 3 (remainder of x divided by 3)
+#x **= 3 (raise x to the power of 3)
